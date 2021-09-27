e7bba0633fea9a95811ac7c8f942f1cc5db96556
zhangshuaining <smart_ning@163.com>
2021-09-27 16:16:20 +0800

    login新增图片验证

--- src/views/Login/Index.vue
+++ src/views/Login/Index.vue
@@ -5,9 +5,9 @@
       <div class="container__form container--signup">
         <form action="#" class="form" id="form1">
           <h2 class="form__title">注册</h2>
-          <input type="text" placeholder="用户名" v-model="name" class="input" />
-          <input type="email" placeholder="邮箱" v-model="email" class="input" />
-          <input type="password" placeholder="密码" v-model="password" class="input" />
+          <input type="text" placeholder="用户名" v-model="userRegister.name" class="input" />
+          <input type="email" placeholder="邮箱" v-model="userRegister.email" class="input" />
+          <input type="password" placeholder="密码" v-model="userRegister.password" class="input" />
           <button class="btn" @click.prevent="register()">注册</button>
         </form>
       </div>
@@ -16,8 +16,8 @@
       <div class="container__form container--signin">
         <form action="#" class="form" id="form2">
           <h2 class="form__title">登录</h2>
-          <input type="text" placeholder="账号" class="input" v-model="account" />
-          <input type="password" placeholder="密码" class="input" v-model="pwd" />
+          <input type="text" placeholder="账号" class="input" v-model="userLogin.account" />
+          <input type="password" placeholder="密码" class="input" v-model="userLogin.pwd" />
           <a href="#" class="link">忘记密码?</a>
           <button class="btn" @click.prevent="login()">登录</button>
         </form>
@@ -36,54 +36,72 @@
       </div>
       <i class="el-icon-edit"></i>
     </div>
+    <Vcode :show="isShow" :imgs="imgs" @fail="onFail" @success="onSuccess" @close="onClose" />
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, toRefs, ref, reactive } from 'vue';
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
+import Vcode from "vue3-puzzle-vcode";
+import Img from '@/assets/Vcode.png'
 export default defineComponent({
+  components: {
+    Vcode
+  },
   setup() {
     const router = useRouter()
-    const userLogin = reactive({
-      account: 'admin',
-      pwd: 'admin'
-    })
-    const isActive = ref(false)
-    const userRegister = reactive({
-      name: '',
-      email: '',
-      password: ''
+    const dataMap = reactive({
+      isShow: false,
+      isActive: false,
+      userLogin: {
+        account: 'admin',
+        pwd: 'admin'
+      },
+      userRegister: {
+        name: '',
+        email: '',
+        password: ''
+      },
+      imgs:[Img]
     })
     const methods = reactive({
       login() {
-        const { account, pwd } = userLogin
+        const { account, pwd } = dataMap.userLogin
         console.log('登陆', account, pwd)
         if (account === '' && pwd === '') {
           console.log('请输入账号密码');
           return
         } else {
-          router.push('/dashboard')
+          dataMap.isShow = true
         }
       },
       signUpBtn() {
-        isActive.value = true
+        dataMap.isActive = true
       },
       signInBtn() {
-        isActive.value = false
+        dataMap.isActive = false
       },
       register() {
-        const { name, email, password } = userRegister
+        const { name, email, password } = dataMap.userRegister
         if (name && email && password) console.log('注册成功')
         else {
           console.log('注册失败')
         }
+      },
+      onClose(){
+        dataMap.isShow = false
+      },
+      onFail(){
+        console.log('验证失败');
+      },
+      onSuccess(){
+        methods.onClose()
+        router.push('/dashboard')
       }
     })
     return {
-      isActive,
       ...toRefs(methods),
-      ...toRefs(userLogin),
-      ...toRefs(userRegister)
+      ...toRefs(dataMap)
     };
   },
 });
